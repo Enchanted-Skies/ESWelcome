2254d4141e13c3552604f06a08eab41e33103495
Alex <alexzatstone@gmail.com>
2021-06-08 19:26:26 +0100

    added minedown support

--- src/main/java/me/xemor/eswelcome/ESJoin.java
+++ src/main/java/me/xemor/eswelcome/ESJoin.java
@@ -1,7 +1,7 @@
 package me.xemor.eswelcome;
 
+import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -9,64 +9,72 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
+import de.themoep.minedown.MineDown;
 
+import java.util.Arrays;
 import java.util.List;
+import static java.util.Objects.requireNonNull;
 
 public class ESJoin implements Listener {
 
-    private Plugin plugin;
-    String firstTimeWelcomeMessage;
-    String welcomeMessage;
-    List<String> firstTimePersonalMessages;
-    List<String> personalMessages;
+    private final Plugin plugin;
+
+    private String firstTimeWelcomeMessage;
+    private String welcomeMessage;
+    private List<String> firstTimePersonalMessages;
+    private List<String> personalMessages;
+    private static BaseComponent[] prefix;
+    private int playerCount = Bukkit.getServer().getOfflinePlayers().length;
+
 
     public ESJoin(ESWelcome pl) {
         plugin = pl; //Set Class's Variable plugin to pl
         loadConfig();
     }
 
-    public void loadConfig() {
-        welcomeMessage = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("welcomeMessage"));
-        firstTimeWelcomeMessage = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("firstTimeWelcomeMessage"));
+    public void loadConfig() { // Load all messages from the config + prefix
+        prefix = MineDown.parse(requireNonNull(plugin.getConfig().getString("commandPrefix")));
+        welcomeMessage = requireNonNull(plugin.getConfig().getString("welcomeMessage"));
+        firstTimeWelcomeMessage = requireNonNull(plugin.getConfig().getString("firstTimeWelcomeMessage"));
         personalMessages = plugin.getConfig().getStringList("personalMessages");
-        personalMessages = chatColorify(personalMessages);
         firstTimePersonalMessages = plugin.getConfig().getStringList("firstTimePersonalMessages");
-        firstTimePersonalMessages = chatColorify(firstTimePersonalMessages);
     }
 
-    public List<String> chatColorify(List<String> list) {
-        for (int i = 0; i < list.size(); i++) {
-            String colorifed = ChatColor.translateAlternateColorCodes('&', list.get(i));
-            list.set(i, colorifed);
-        }
-        return list;
-    }
-
+    //sets join message and possibly sends the player a personal message
     @EventHandler(priority = EventPriority.HIGHEST) //onJoin do ...
     public void onJoin(PlayerJoinEvent e) {
-        Player player = e.getPlayer(); //Make Player variable
-        int offlinePlayers = Bukkit.getServer().getOfflinePlayers().length;
-        int playerCount = offlinePlayers;
+        final Player player = e.getPlayer(); //Make Player variable
+        // override the join message
+        e.setJoinMessage(null);
         if (!player.hasPlayedBefore()) { //If the player hasn't played before
-            String tempWelcomeMessage = firstTimeWelcomeMessage;
-            tempWelcomeMessage = tempWelcomeMessage.replace("{PLAYER}", player.getName());
-            tempWelcomeMessage = tempWelcomeMessage.replace("{COUNT}", Integer.toString(playerCount));
-            e.setJoinMessage(tempWelcomeMessage);
-            for (String personalMessage : firstTimePersonalMessages) {
-                personalMessage = personalMessage.replace("{PLAYER}", player.getName());
-                personalMessage = personalMessage.replace("{COUNT}", Integer.toString(playerCount));
-                player.sendMessage(personalMessage);
-            }
+            playerCount++;
+            sendGeneralJoinMessage(firstTimeWelcomeMessage, player);
+            sendPersonalJoinMessage(firstTimePersonalMessages,player);
+        } else {
+            sendGeneralJoinMessage(welcomeMessage, player);
+            sendPersonalJoinMessage(personalMessages, player);
         }
-        else {
-            String tempWelcome = welcomeMessage;
-            tempWelcome = tempWelcome.replace("{PLAYER}", player.getName());
-            e.setJoinMessage(tempWelcome);
-            for (String personalMessage : personalMessages) {
-                String tempPersonalMessage = personalMessage.replace("{PLAYER}", player.getName());
-                player.sendMessage(tempPersonalMessage);
-            }
-        }
+    }
+
+    private void sendPersonalJoinMessage(List<String> messages, Player player) {
+        //For every personal message, minedown it, replace the placeholders, then send the Basecomponents to the player.
+        messages.stream().map(
+                it -> new MineDown(it).replaceFirst(true)
+                        .replace("PLAYER", player.getDisplayName(), "COUNT", Integer.toString(playerCount))
+                        .toComponent())
+                .forEachOrdered(it -> player.spigot().sendMessage(it));
+    }
+
+    private void sendGeneralJoinMessage(String message, Player player) {
+        // map message to basecomponent via minedown then send to player + console.
+        final BaseComponent[] baseComponents = new MineDown(message).replaceFirst(true).replace("PLAYER", player.getDisplayName(), "COUNT", Integer.toString(playerCount)).toComponent();
+        // IGNORE LINT, type erasure fails compile time if replaced with method reference
+        Bukkit.getConsoleSender().sendMessage(Arrays.stream(baseComponents).map(it -> it.toPlainText()).toArray(String[]::new));
+        Bukkit.getOnlinePlayers().forEach(it -> it.spigot().sendMessage(baseComponents));
+    }
+
+    public static BaseComponent[] getPrefix() {
+        return prefix;
     }
 
     @EventHandler
--- src/main/java/me/xemor/eswelcome/ESWelcomeCommands.java
+++ src/main/java/me/xemor/eswelcome/ESWelcomeCommands.java
@@ -1,11 +1,17 @@
 package me.xemor.eswelcome;
 
+import static me.xemor.eswelcome.ESJoin.getPrefix;
+
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ESWelcomeCommands implements CommandExecutor, TabExecutor {
@@ -19,11 +25,18 @@ public class ESWelcomeCommands implements CommandExecutor, TabExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender.hasPermission("eswelcome.admin")) {
             if (args.length == 0) {
-                sender.sendMessage("You need to put an argument! Example: reload.");
+                final BaseComponent[] messages = new BaseComponent[getPrefix().length + 1];
+                messages[getPrefix().length]= new TextComponent("You need to put an argument! Example: reload.");
+                sender.spigot().sendMessage(messages);
             }
             if (args[0].equals("reload")) {
                 esWelcome.reload();
-                sender.sendMessage("It has been successfully reloaded!");
+                final BaseComponent[] messages = new BaseComponent[getPrefix().length + 1];
+                messages[getPrefix().length] = new TextComponent("ESWelcome has been successfully reloaded!");
+                sender.spigot().sendMessage(messages);
+                // send a message to the console as well
+                // IGNORE LINT, type erasure fails compile time if replaced with method reference
+                Bukkit.getConsoleSender().sendMessage(Arrays.stream(messages).map(it -> it.toPlainText()).toArray(String[]::new));
             }
         }
         return true;
@@ -31,7 +44,7 @@ public class ESWelcomeCommands implements CommandExecutor, TabExecutor {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return Arrays.asList(new String[]{"reload"});
+        return Collections.singletonList("reload");
     }
 
 }
