14b593b99f34cdebb77a449ef9e580be0fbdda1a
Xemorr <31805746+Xemorr@users.noreply.github.com>
2021-06-21 23:42:41 +0100

    Current version, added PlaceholderAPI and MineDown support.

--- src/main/java/me/xemor/eswelcome/ESWelcomeCommands.java
+++ src/main/java/me/xemor/eswelcome/ESWelcomeCommands.java
@@ -1,17 +1,11 @@
 package me.xemor.eswelcome;
 
-import static me.xemor.eswelcome.ESJoin.getPrefix;
-
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class ESWelcomeCommands implements CommandExecutor, TabExecutor {
@@ -25,18 +19,11 @@ public class ESWelcomeCommands implements CommandExecutor, TabExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender.hasPermission("eswelcome.admin")) {
             if (args.length == 0) {
-                final BaseComponent[] messages = new BaseComponent[getPrefix().length + 1];
-                messages[getPrefix().length]= new TextComponent("You need to put an argument! Example: reload.");
-                sender.spigot().sendMessage(messages);
+                sender.sendMessage("You need to put an argument! Example: reload.");
             }
             if (args[0].equals("reload")) {
                 esWelcome.reload();
-                final BaseComponent[] messages = new BaseComponent[getPrefix().length + 1];
-                messages[getPrefix().length] = new TextComponent("ESWelcome has been successfully reloaded!");
-                sender.spigot().sendMessage(messages);
-                // send a message to the console as well
-                // IGNORE LINT, type erasure fails compile time if replaced with method reference
-                Bukkit.getConsoleSender().sendMessage(Arrays.stream(messages).map(it -> it.toPlainText()).toArray(String[]::new));
+                sender.sendMessage("It has been successfully reloaded!");
             }
         }
         return true;
@@ -44,7 +31,7 @@ public class ESWelcomeCommands implements CommandExecutor, TabExecutor {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return Collections.singletonList("reload");
+        return Arrays.asList(new String[]{"reload"});
     }
 
 }
